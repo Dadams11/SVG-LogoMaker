d055168a95e2e80617b4dcb1bec8066c09132d5f
Dadams11 <adams.1743@osu.edu>
2023-07-13 22:58:28 -0400

    added two more test for shapes.test.js file

--- shapes.test.js
+++ shapes.test.js
@@ -1,13 +1,19 @@
 const { Circle, Triangle, Square } = require('./shapes.js');
 
 describe('render method', () => {
-  test('returns SVG string with shape color', () => {
+  test('Circle - returns SVG string with shape color', () => {
     const circle = new Circle('red');
-    const triangle = new Triangle('blue');
-    const square = new Square('green');
-
     expect(circle.render()).toBe(`<svg><circle cx="0" cy="0" r="10" fill="red"></circle></svg>`);
+  });
+
+  test('Triangle - returns SVG string with shape color', () => {
+    const triangle = new Triangle('blue');
     expect(triangle.render()).toBe(`<svg><polygon points="0,0 -4,8 4,8" fill="blue"></polygon></svg>`);
+  });
+
+  test('Square - returns SVG string with shape color', () => {
+    const square = new Square('green');
     expect(square.render()).toBe(`<svg><rect width="6" height="6" fill="green"></rect></svg>`);
   });
 });
+
